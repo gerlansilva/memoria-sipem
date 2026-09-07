cc559d48e9b3a79f709d964f23c8504f292575d5
Gerlan Silva <50636844+gerlansilva@users.noreply.github.com>
2026-09-07 20:44:37 -0300

    Adiciona filiações validadas do V SIPEM GT01-02

---
+++ data/v2012-affiliations-gt01-02.js
@@ -0,0 +1,180 @@
+// V SIPEM (2012) — filiações GT01-02 preservadas conforme os PDFs.
+window.SIPEM_V2012_AFFILIATIONS = window.SIPEM_V2012_AFFILIATIONS || {};
+Object.assign(window.SIPEM_V2012_AFFILIATIONS, {
+  "CC00139332804_A.pdf": [
+    [
+      "Edvonete Souza de Alencar",
+      "Anhanguera Uniban, Brasil"
+    ],
+    [
+      "Angélica da Fontoura Garcia Silva",
+      "Anhanguera Uniban, Brasil"
+    ]
+  ],
+  "CC00625654781_A.pdf": [
+    [
+      "Maria Helena Marques Loth",
+      "Prefeitura de Juiz de Fora, Brasil"
+    ]
+  ],
+  "CC00720610060_A.pdf": [
+    [
+      "Clarissa de Assis Olgin",
+      "Universidade Luterana do Brasil, Brasil"
+    ],
+    [
+      "Claudia Lisete Oliveira Groenwald",
+      "Universidade Luterana do Brasil, Brasil"
+    ]
+  ],
+  "CC00779800427_A.pdf": [
+    [
+      "Edelweis Jose Tavares Barbosa",
+      "Universidade Estadual da Paraíba- UEPB, Brasil"
+    ],
+    [
+      "Abigail Fregni Lins",
+      "Universidade Estadual da Paraíba- UEPB, Brasil"
+    ]
+  ],
+  "CC02875535820_A.pdf": [
+    [
+      "Edda Curi",
+      "Universidade Cruzeiro do Sul, Brasil"
+    ],
+    [
+      "Cintia Aparecida Bento dos Santos",
+      "Universidade Cruzeiro do Sul, Brasil"
+    ]
+  ],
+  "CC03272362800_A.pdf": [
+    [
+      "Clélia Maria Ignatius Nogueira",
+      "Centro Universitário de Maringá - CESUMAR, Brasil"
+    ],
+    [
+      "Regina Maria Pavanello",
+      "Universidade Estadual de Maringá, Brasil"
+    ],
+    [
+      "Lucilene Lusia Adorno de Oliveira",
+      "Secretaria de Estado da Educação do Paraná, Brasil"
+    ]
+  ],
+  "CC07494595813_A.pdf": [
+    [
+      "Gilda Lisbôa Guimarães",
+      "Universidade Federal de Pernambuco, Brasil"
+    ]
+  ],
+  "CC08059565656_A.pdf": [
+    [
+      "Bruna Karla Silva Reginaldo",
+      "Universidade Federal de Minas Gerais, Brasil"
+    ]
+  ],
+  "CC09715887830_A.pdf": [
+    [
+      "Jeferson da Silva Gonçalves",
+      "Universidade Bandeirante de São Paulo, Brasil"
+    ],
+    [
+      "Monica Karrer",
+      "Universidade Bandeirante de São Paulo, Brasil"
+    ]
+  ],
+  "CC17829682453_A.pdf": [
+    [
+      "Lúcia de Fatima Araújo",
+      "Universidade Federal Rural de Pernambuco,Brasil"
+    ],
+    [
+      "Abraão Juvencio de Araujo",
+      "Universidade Federal de Pernambuco, Brasil"
+    ]
+  ],
+  "CC18035807803_A.pdf": [
+    [
+      "Josiane Acacia de Oliveira Marques",
+      "Universidade Federal de São Paulo – UNIFESP – GHEMAT, Brasil."
+    ]
+  ],
+  "CC22057969843_A.pdf": [
+    [
+      "Ricardo Scucuglia Rodrigues da Silva",
+      "Western University, Canada & GPIMEM, Brazil"
+    ]
+  ],
+  "CC23993901053_A.pdf": [
+    [
+      "Loraci Maria Birck",
+      "3ª Coordenadoria de Educação/RS"
+    ],
+    [
+      "Carmen Teresa Kaiber",
+      "Universidade Luterana do Brasil – ULBRA"
+    ]
+  ],
+  "CC24010286172_A.pdf": [
+    [
+      "Ana Maria Porto Nascimento",
+      "Universidade Federal da Bahia - Campus de Barreiras, Brasil"
+    ],
+    [
+      "Adelaide Batista Corado Santos",
+      "Secretaria Municipal de Educação de Barreiras, Brasil"
+    ]
+  ],
+  "CC43277748715_A.pdf": [
+    [
+      "Mônica Cerbella Freire Mandarino",
+      "Universidade federal do Estado do Rio de Janeiro –UNIRIO"
+    ]
+  ],
+  "CC52976777500_A.pdf": [
+    [
+      "Eurivalda Ribeiro dos Santos Santana",
+      "Universidade Estadual de Santa Cruz, Brasil"
+    ],
+    [
+      "Fernanda de Oliveira Soares Taxa Amaro",
+      "Pontifícia Universidade Católica de Campinas, Brasil"
+    ],
+    [
+      "Ana Virginia de Almeida Luna",
+      "Universidade Estadual de Feira de Santana, Brasil"
+    ]
+  ],
+  "CC64107000168_B.pdf": [
+    [
+      "Regina da Silva Pina Neves",
+      "Instituto Nacional de Estudos e Pesquisas Educacionais (INEP1), Brasil"
+    ],
+    [
+      "Maria Helena Fávero",
+      "Instituto de Psicologia/Universidade de Brasília, Brasil"
+    ]
+  ],
+  "CC66657466404_A.pdf": [
+    [
+      "Cristiane Azevêdo dos Santos Pessoa",
+      "Universidade Federal de Pernambuco, Brasil"
+    ],
+    [
+      "Rute Elizabete de Souza Rosa Borba",
+      "Universidade Federal de Pernambuco, Brasil"
+    ]
+  ],
+  "CC79114563720_A.pdf": [
+    [
+      "Ana Maria Carneiro Abrahão",
+      "Universidade Federal do Estado do Rio de Janeiro (UNIRIO), Brasil"
+    ]
+  ],
+  "CC84996560991_A.pdf": [
+    [
+      "Claudia Laus Angelo",
+      "Universidade Federal do Pampa, campus Bagé-RS, Brasil"
+    ]
+  ]
+});
